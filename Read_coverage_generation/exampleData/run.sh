98272c271f1fb2dd87fb0071a4b9478f06073312
ay-lab <ferhatay@lji.org>
2018-02-04 01:36:32 -0800

    Update run.sh

--- Read_coverage_generation/exampleData/run.sh
+++ Read_coverage_generation/exampleData/run.sh
@@ -1,4 +1,6 @@
 ## Example to create perREfragStats and F_GC_MAP.bed files from Hi-C pro output. R1 is the forward read and R2 is the reverse read mapped file ## 
+tar -xzvf MCF7.R1.tar.gz
+tar -xzvf MCF7.R2.tar.gz
 mkdir allMap2FragmentsOutput
 python ../../scripts/mergeSAM-singletons.py -f MCF7.R1_hg19.bwt2merged.bam -r MCF7.R2_hg19.bwt2merged.bam -o MCF7.bwt2pairs.withSingles.mapq30.bam -v --single -q 30
 python ../../scripts/mapped_2hic_fragments.py -f ../../scripts/HindIII_resfrag_hg19.bed -s 100 -l 800 -d 1000 -r MCF7.bwt2pairs.withSingles.mapq30.bam -o allMap2FragmentsOutput --all -v
